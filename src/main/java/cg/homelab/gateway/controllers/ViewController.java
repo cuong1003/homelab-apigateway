5cf522bfa76cb19416f184f7c3715de3478ab571
alone nugget <fakeymeo@gmail.com>
2026-01-05 14:57:47 +0700

    first commit

---
+++ src/main/java/cg/homelab/gateway/controllers/ViewController.java
@@ -0,0 +1,27 @@
+package cg.homelab.gateway.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class ViewController {
+    @GetMapping("/login")
+    public String loginPage() {
+        return "login";
+    }
+    @GetMapping("/")
+    public String landingPage() {
+        return "redirect:/login";
+    }
+    @GetMapping("/dashboard")
+    public String dashboardPage() {
+        return "dashboard";
+    }
+    @PostMapping("/signup")
+    public String signup(@RequestParam String username, @RequestParam String password) {
+
+        return "redirect:/login";
+    }
+}
